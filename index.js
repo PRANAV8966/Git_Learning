732eb751120b76fb94a7271d619387554ff3e042
Pranav <pranav.wan706@gmail.com>
2024-10-09 23:31:15 +0530

    github linkage

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-console.log("hello world");
-console.log("this is the change");
-console.log("new change);
+let x = "hey there!!";
+console.log("Hellow world!!);
+function Writefile(url) {
+	return new Promise(function process(resolve, reject){
+		console.log("started downloading");
+		setTimeout(function fun() {
+			let data = "dummy data";
+			resolve(data);
+		},3000)
+	});
+
+let y = writefile("www.google.com);
+console.log(x);
 
